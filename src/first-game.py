50a84a61b678e6fc42e16a8891f8ad2a40410259
Cole Heflin <cvheflin@gmail.com>
2021-04-21 20:07:16 -0700

    adding link to tutorial

--- src/first-game.py
+++ src/first-game.py
@@ -34,15 +34,37 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(5, 0)
 
-        # Keep player on the screen
-        # if self.rect.left < 0:
-        #     self.rect.left = 0
-        # if self.rect.right > SCREEN_WIDTH:
-        #     self.rect.right = SCREEN_WIDTH
-        # if self.rect.top <= 0:
-        #     self.rect.top = 0
-        # if self.rect.bottom >= SCREEN_HEIGHT:
-        #     self.rect.bottom = SCREEN_HEIGHT
+        Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+# Define the enemy object by extending pygame.sprite.Sprite
+# The surface you draw on the screen is now an attribute of 'enemy'
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    # Move the sprite based on speed
+    # Remove the sprite when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 # Initialize pygame
 pygame.init()
 
@@ -86,3 +108,5 @@ while running:
 
     # Update the display
     pygame.display.flip()
+
+https://realpython.com/pygame-a-primer/
